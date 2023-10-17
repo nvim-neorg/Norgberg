ed467ef2b7cf213442cbc350b1ce1b444fda04a5
Vhyrro <vhyrro@gmail.com>
2023-10-17 16:28:38 +0200

    fix: surrealdb with filepaths

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use norgopolis_module::{
     invoker_service::Service, module_communication::MessagePack, Module, Status,
 };
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use tokio_stream::wrappers::ReceiverStream;
 
 use surrealdb::engine::any::{connect, Any as AnySurrealConnection};
@@ -13,7 +13,7 @@ struct Norgberg {
 }
 
 impl Norgberg {
-    async fn new(file: &PathBuf) -> Result<Self> {
+    async fn new(file: &Path) -> Result<Self> {
         Ok(Norgberg {
             connection: connect(file.to_str().unwrap_or("memory")).await?,
         })
@@ -36,17 +36,17 @@ impl Service for Norgberg {
 #[tokio::main]
 async fn main() {
     let database_location = if cfg!(debug_assertions) {
-        Path::new("memory").to_owned()
+        "memory".to_owned()
     } else {
         let data_dir = directories::ProjectDirs::from("org", "neorg", "norgberg").expect("Could not grab known data directories, are you running on a non-unix and non-windows system?").data_dir().to_path_buf();
 
         let _ = std::fs::create_dir_all(&data_dir);
 
-        data_dir.join("database.sql")
+        "file://".to_string() + data_dir.join("database.sql").to_str().unwrap()
     };
 
     Module::start(
-        Norgberg::new(&database_location)
+        Norgberg::new(&Path::new(&database_location))
             .await
             .expect("Unable to connect to database!"),
     )
